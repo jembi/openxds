fb7c8edda503008aacb14d38633d9369d3abf412
odysseas <odysseas>
2009-06-02 22:39:15 +0000

    Initial check-in of development environment and framework for the implementation of the OpenXDS server.

---
+++ core/src/main/java/org/openhealthexchange/openxds/service/impl/LookupManagerImpl.java
@@ -0,0 +1,42 @@
+package org.openhealthexchange.openxds.service.impl;
+
+import org.openhealthexchange.openxds.dao.LookupDao;
+import org.openhealthexchange.openxds.model.LabelValue;
+import org.openhealthexchange.openxds.model.Role;
+import org.openhealthexchange.openxds.service.LookupManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Implementation of LookupManager interface to talk to the persistence layer.
+ *
+ * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
+ */
+public class LookupManagerImpl extends UniversalManagerImpl implements LookupManager {
+    private LookupDao dao;
+
+    /**
+     * Method that allows setting the DAO to talk to the data store with.
+     * @param dao the dao implementation
+     */
+    public void setLookupDao(LookupDao dao) {
+        super.dao = dao;
+        this.dao = dao;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<LabelValue> getAllRoles() {
+        List<Role> roles = dao.getRoles();
+        List<LabelValue> list = new ArrayList<LabelValue>();
+
+        for (Role role1 : roles) {
+            list.add(new LabelValue(role1.getName(), role1.getName()));
+        }
+
+        return list;
+    }
+}
---
+++ web/src/main/java/org/openhealthexchange/openxds/App.java
@@ -0,0 +1,7 @@
+package org.openhealthexchange.openxds;
+
+public class App {
+    public static String getHello() {
+        return "Hello";
+    }
+}
