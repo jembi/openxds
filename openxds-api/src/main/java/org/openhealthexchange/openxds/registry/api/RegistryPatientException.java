0a68f433f3b773d136dafc096ae7514b7d2c2cb4
wenzhi <wenzhi>
2009-06-08 22:22:12 +0000

    Initial code drop

---
+++ openxds-api/src/main/java/org/openhealthexchange/openxds/registry/api/RegistryPatientException.java
@@ -0,0 +1,61 @@
+/**
+ *  Copyright © 2009 Misys plc, Sysnet International, Medem and others
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ *  Contributors:
+ *     Misys plc - Initial API and Implementation
+ */
+package org.openhealthexchange.openxds.registry.api;
+
+/**
+ * This exception is generated when there is a problem
+ * with XDS Registry Patient operations
+ *
+ * @author <a href="mailto:wenzhi.li@misys.com">Wenzhi Li</a>
+ *
+ */
+public class RegistryPatientException extends Exception {
+	
+	private static final long serialVersionUID = 9202229251640160669L;
+				
+	/**
+	 * Creates a new RegistryPatientException.
+	 * 
+	 * @param msg a description of the problem
+	 */
+	public RegistryPatientException(String msg) {
+		super(msg);
+	}
+
+	/**
+	 * Creates a new RegistryPatientException.
+	 * 
+	 * @param msg a description of the problem
+	 * @param cause the embedded Throwable
+	 */
+    public RegistryPatientException(String msg, Throwable cause){
+        super(msg, cause);
+    }
+
+    
+	/**
+	 * Creates a new RegistryPatientException.
+	 * 
+	 * @param cause the embedded Throwable
+	 */
+    public RegistryPatientException(Throwable cause) {
+        super(cause);
+    }
+
+}
---
+++ openxds-api/src/main/java/org/openhealthexchange/openxds/repository/api/RepositoryRequestContext.java
@@ -0,0 +1,29 @@
+/**
+ *  Copyright © 2009 Misys plc, Sysnet International, Medem and others
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ *  Contributors:
+ *     Misys plc - Initial API and Implementation
+ */
+package org.openhealthexchange.openxds.repository.api;
+
+/**
+ * This classes defines a repository context that are required for
+ * the repository operations of the repository manager service.
+ * 
+ * @author <a href="mailto:wenzhi.li@misys.com">Wenzhi Li</a>
+ */
+public class RepositoryRequestContext {
+	
+}
