2047437a8958108b85a91ab7c46d8c820e8bca21
wenzhi <wenzhi>
2009-07-14 22:02:29 +0000

    1. Added properties like registry client connection url and repositoryUniqueId etc. 2. Passed the actor object to each WS handler class so that each connection of the actor is available in the WS handler.

--- openxds/iheos/src/main/java/gov/nist/registry/ws/config/Repository.java
+++ openxds/iheos/src/main/java/gov/nist/registry/ws/config/Repository.java
@@ -4,7 +4,13 @@ import gov.nist.registry.common2.exception.XdsConfigurationException;
 import gov.nist.registry.common2.registry.Properties;
 import gov.nist.registry.ws.serviceclasses.XdsService;
 
-import java.util.ArrayList;
+import org.apache.commons.httpclient.protocol.Protocol;
+import org.openhealthexchange.openxds.configuration.ModuleManager;
+import org.openhealthexchange.openxds.repository.api.IXdsRepositoryManager;
+
+import com.misyshealthcare.connect.net.IConnectionDescription;
+import com.misyshealthcare.connect.net.SecureConnectionDescription;
+import com.misyshealthcare.connect.net.SecureSocketFactory;
 
 public class Repository {
 	static public String getBaseDirectory() {
@@ -25,8 +31,31 @@ public class Repository {
 		return "http://localhost:9080/" + XdsService.technicalFramework + "/services/xdsregistryainternal";
 	}
 
+	
+	/**
+	 * Get the Endpoint of this SOAP message.
+	 * 
+	 * @return the URL string of the web service
+	 */
+	static public String getRegisterTransactionEndpoint(IConnectionDescription connection) {
+		String host = connection.getHostname();
+		int port = connection.getPort();
+		boolean isSecure = connection.isSecure();
+		String url = "http://";
+		if(isSecure) {
+			url="https://";
+			//TODO: need to use local rather than global setting
+			Protocol.registerProtocol("https", new Protocol("https", 
+			  new SecureSocketFactory((SecureConnectionDescription)connection), port));
+		}
+		 
+		url+= host + ":" + port + connection.getUrlPath();
+		return url;
+	}
+	
 	static public String getRepositoryUniqueId() {
-		return Properties.loader().getString("repository_unique_id");
+		IXdsRepositoryManager rm = (IXdsRepositoryManager)ModuleManager.getInstance().getBean("repositoryManager");
+		return rm.getRepositoryUniqueId();
 	}
 
 }
