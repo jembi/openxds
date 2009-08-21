3587ff70b153fa73f4c5c72e33f6fefc69c9eb6b
Rasakannu <Rasakannu>
2009-08-21 11:41:14 +0000

    Added repository spring configuration file class path in ModuleManager class, Added repository spring configuration & DB properties file, Added repository hbm file & pojo class, Added XdsRepositoryManagerImpl,XdsReposityManagerDao & XdsReposityManagerDaoImpl class.

---
+++ openxds-repository/src/main/java/org/openhealthexchange/openxds/repository/dao/XdsRepositoryManagerDao.java
@@ -0,0 +1,29 @@
+/**
+ *  Copyright © 2009 Misys plc, Sysnet International, Medem and others
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ *  Contributors:
+ *     Misys plc - Initial API and Implementation
+ */    
+package org.openhealthexchange.openxds.repository.dao;
+
+/**
+ * This class provides a Relational Database XDS repository manager implementation.
+ *
+ * @author <a href="mailto:Rasakannu.Palaniyandi@misys.com">Raja</a>
+ * 
+ */
+public class XdsRepositoryManagerDao {
+
+}
