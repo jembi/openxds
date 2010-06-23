39d7b1ac0eaa784dba0dc425a1e29e6c33a87b31
venkata_p <venkata_p>
2010-06-23 09:26:14 +0000

    Added interface to filter document metadata

---
+++ openxds/openxds-api/src/main/java/org/openhealthtools/openxds/registry/api/ResourceingFilter.java
@@ -0,0 +1,43 @@
+/**
+ *  Copyright (c) 2009-2010 Misys Open Source Solutions (MOSS) and others
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ *  Contributors:
+ *    Misys Open Source Solutions - initial API and implementation
+ *    -
+ */
+
+package org.openhealthtools.openxds.registry.api;
+
+import java.util.List;
+
+import org.apache.axiom.om.OMElement;
+
+/**
+* This interface defines the operations to filter resources
+* 
+* @author <a href="mailto:venkata.pragallapati@misys.com">Venkat</a>
+* 
+*/
+public interface ResourceingFilter {
+	
+	/**
+	 * It filters documents for user based on user's priviliges 
+	 * @param extrinsicObjects
+	 * @param assertion
+	 * @return
+	 */
+	public List<OMElement> filterExtrinsicObjects(List<OMElement> extrinsicObjects, OMElement userAssertion) throws Exception;
+
+}
