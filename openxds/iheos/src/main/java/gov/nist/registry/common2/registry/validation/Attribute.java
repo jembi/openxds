67f4098c613c06947d0b721c4061fe16a1a6d759
wenzhi <wenzhi>
2011-01-17 23:40:35 +0000

    Fixed a typo

--- openxds/iheos/src/main/java/gov/nist/registry/common2/registry/validation/Attribute.java
+++ openxds/iheos/src/main/java/gov/nist/registry/common2/registry/validation/Attribute.java
@@ -170,7 +170,7 @@ public class Attribute {
 			String value = value_ele.getText();
 			List<String> errs = validate_XON(value);
 			for (String err : errs) {
-				err("authorInstituion: " + err);
+				err("authorInstitution: " + err);
 			}
 		}
 	}
