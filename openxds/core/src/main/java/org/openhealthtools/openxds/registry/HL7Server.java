da09574d3112d195f0b1454a153390b6b6aad93b
anil <anil>
2011-04-11 15:41:37 +0000

    Updated copyright header

--- openxds/core/src/main/java/org/openhealthtools/openxds/registry/HL7Server.java
+++ openxds/core/src/main/java/org/openhealthtools/openxds/registry/HL7Server.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright (c) 2009-2010 Misys Open Source Solutions (MOSS) and others
+ *  Copyright (c) 2009-2011 Misys Open Source Solutions (MOSS) and others
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
  *    Misys Open Source Solutions - initial API and implementation
  *    -
  */
+
 package org.openhealthtools.openxds.registry;
 
 import java.io.InterruptedIOException;
--- openxds/web/src/main/webapp/index.jsp
+++ openxds/web/src/main/webapp/index.jsp
@@ -1,6 +1,6 @@
 <%--
 
-     Copyright (c) 2009-2010 Misys Open Source Solutions (MOSS) and others
+     Copyright (c) 2009-2011 Misys Open Source Solutions (MOSS) and others
 
      Licensed under the Apache License, Version 2.0 (the "License");
      you may not use this file except in compliance with the License.
